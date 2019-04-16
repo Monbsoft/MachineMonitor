068357947f2fa1f75f5d30115b2fe5983e7411e1
oliver254 <oliver254@hotmail.fr>
2019-04-16 14:41:38 +0200

    [#1] : readme page created

---
+++ README.md
@@ -0,0 +1,10 @@
+# MachineMonitor
+[![Build status](https://monbsoft.visualstudio.com/MachineMonitor/_apis/build/status/MachineMonitor-CI)](https://monbsoft.visualstudio.com/MachineMonitor/_build/latest?definitionId=5)
+
+Machine Monitor is a system monitor to display resources and performances of your machine (CPU, Memory and GPU). 
+
+
+## Credits
+* [windows-toolkit/WindowsCommunityToolkit](https://github.com/windows-toolkit/WindowsCommunityToolkit), The Windows Community Toolkit is a collection of helper functions, custom controls, and app services. 
+
+
--- src/MachineMonitor/ViewModels/MainViewModel.cs
+++ src/MachineMonitor/ViewModels/MainViewModel.cs
@@ -8,8 +8,10 @@ namespace Monbsoft.MachineMonitor.ViewModels
     public class MainViewModel : ViewModelBase
     {
         private readonly PerformanceCounter _cpuCounter;
+        private readonly PerformanceCounter _diskCounter;
         private readonly PerformanceCounter _memoryCounter;
         private double _cpu;
+        private double _disk;
         private double _ram;
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
@@ -18,10 +20,11 @@ namespace Monbsoft.MachineMonitor.ViewModels
         {
             _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             _memoryCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            _diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Read Time", "_Total");
         }
 
         /// <summary>
-        /// Gets or sets the percentage of the cpu.
+        /// Gets or sets the percentage of the cpu usage.
         /// </summary>
         public double Cpu
         {
@@ -29,19 +32,34 @@ namespace Monbsoft.MachineMonitor.ViewModels
             set { Set(ref _cpu, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the percentage of the disk usage.
+        /// </summary>
+        public double Disk
+        {
+            get { return _disk; }
+            set { Set(ref _disk, value); }
+        }
+        /// <summary>
+        /// Gets or sets the percentage of the memory usage.
+        /// </summary>
         public double Ram
         {
             get { return _ram; }
             set { Set(ref _ram, value); }
         }
-
         public void Refresh()
         {
             Cpu = _cpuCounter.NextValue();
             Ram = _memoryCounter.NextValue();
-            Console.WriteLine($"CPU = {_cpu} RAM = {_ram}");
+            Disk = _diskCounter.NextValue();
+            Test();
+        }
 
-            
+        private void Test()
+        {
+            //PerformanceCounter bandwidthCounter = new PerformanceCounter("Network Interface", "Current Bandwidth", networkCard);
+            //float bandwidth = bandwidthCounter.NextValue();
         }
     }
 }
